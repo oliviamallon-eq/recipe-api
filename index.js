98452bac2ec7896ac2a2f339bd4d48cf3aace536
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-05 16:43:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/posts', (req,res) => {
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
