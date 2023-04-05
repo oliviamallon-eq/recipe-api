ee7803e4a9ebb4df0d122310e5d9e0a0aac48a31
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-05 16:40:58 +0100

    update res

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/posts', (req,res) => {
     res.json(posts)
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.get('/',(req,res) => {
     res.send('Api is running.')
 })
