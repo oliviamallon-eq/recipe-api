4b84947e5f73f248796075d90acf8e80c1c62225
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-05 16:13:13 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # recipe-api
 My Recipe API
-Watching how this changes
+
+Welcome to my Recipe API, this is where I will store the name and ingredients of recipes (potentially more information to come) so I can use this information in apps etc in the future. 
---
+++ posts.js
@@ -0,0 +1,42 @@
+module.exports =[
+    {
+        "id": 0,
+        "name": "Chilli con carne",
+        "serves": "6-8 people",
+        "img": "some random URL",
+        "ingredients": [
+          "1.5kg mince steak - lean 5% fat",
+          "2 onions",
+          "2 red/green peppers",
+          "1 tin red kidney beans",
+          "1 tin chopped tomatoes",
+          "2 tablespoon tomato puree",
+          "2 tablespoon cornflour",
+          "1 teaspoon cumin",
+          "1 teaspoon paprika",
+          "2 tablespoon chilli powder",
+          "1 teaspoon oregano",
+          "1 beef oxo cube"
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Chicken Curry",
+        "serves": "6-8 people",
+        "img": "some random URL",
+        "ingredients": [
+          "3-4 chicken breasts",
+          "1 jar/packet of curry sauce (your choice)",
+          "1 onion",
+          "2 garlic cloves",
+          "1 red chilli / chilli powder",
+          "2-3 red/yellow/orange peppers",
+          "1 courgette",
+          "1 packet/tin baby plum tomatoes",
+          "2 diced sweet potatoes",
+          "1 packet mushrooms",
+          "1 packet sugar snap peas",
+          "any veg of your choice!"
+        ]
+    }
+]
