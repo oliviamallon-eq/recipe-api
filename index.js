d8403b874b9dc7c993cf14662fa6c6af2d6c3b34
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-06 09:32:10 +0100

    eek

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const posts = require('./posts')
 
 app.get('/posts', (req,res) => {
     res.status(200)
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     res.json(posts)
 })
 
 app.use((req, res, next) => {
     res.status(200)
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     next();
   });
 
