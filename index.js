bbb77189e117db2df0d3ac15c2fcc2f6d30389cd
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-06 09:24:47 +0100

    attempt

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express')
 const app = express()
 const posts = require('./posts')
-const cors = require('cors')
 
 app.get('/posts', (req,res) => {
     res.json(posts)
 })
 
-app.use(cors());
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.get('/',(req,res) => {
     res.send('Api is running.')
