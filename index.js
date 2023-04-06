d84f4fb1af603045df515646a1e80c362dd16461
oliviamallon-eq <olivia.mallon@everquote.com>
2023-04-06 09:28:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 const posts = require('./posts')
 
 app.get('/posts', (req,res) => {
+    res.status(200)
+    res.header('Access-Control-Allow-Origin', '*');
     res.json(posts)
 })
 
 app.use((req, res, next) => {
+    res.status(200)
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
